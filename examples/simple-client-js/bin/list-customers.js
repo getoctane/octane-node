a572c51889cd03d9e553d62d728b6e07f6a837d7
Josh Dolitsky <josh@dolit.ski>
2021-07-15 14:55:22 -0400

    Remove all older examples
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- examples/simple-client-js/bin/list-customers.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-const octane = require("../lib/require-octane-local.js");
-
-octane.customers.list()
-    .then(customers => console.log(customers))
-    .catch(error => console.error(error));
