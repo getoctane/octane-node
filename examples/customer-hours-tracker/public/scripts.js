2153d3a9dc482aee2053ac90c1a1679750f495a3
Josh Dolitsky <josh@dolit.ski>
2021-07-15 16:10:30 -0400

    Creating customers in demo app
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- examples/customer-hours-tracker/public/scripts.js
+++ examples/customer-hours-tracker/public/scripts.js
@@ -1,19 +1,58 @@
-var page = document;
-page.show = function(id) { page.getElementById(id).style['display'] = 'block' };
-page.hide = function(id) { page.getElementById(id).style['display'] = 'none' };
+var get = function(id) { return document.getElementById(id); }
+var show = function(id) { get(id).style['display'] = 'block' };
+var hide = function(id) { get(id).style['display'] = 'none' };
+var reset = function(id) { get(id).value = null };
 
 var loadCustomers = function() {
-    page.show('loading-customers');
+    hide('add-new-customer');
+    show('loading-customers');
     fetch('/api/customers')
         .then(function(response) {
-            return response.json()
+            return response.json();
         })
         .then(function(customers) {
             console.log(customers);
-            page.hide('loading-customers');
+            hide('loading-customers');
+            show('add-new-customer');
         });
+};
+
+var createCustomerInProgress = false;
+var createCustomer = function(name) {
+    if (createCustomerInProgress) {
+        return;
+    }
+    createCustomerInProgress = true;
+    var options = {
+        method: 'POST',
+        body: JSON.stringify({name: name}),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+    fetch('/api/customers', options)
+        .then(function(response) {
+            createCustomerInProgress = false;
+            if (response.status !== 201) {
+                console.log(response);
+                alert('Error creating new customer: ' +
+                    response.status + ': ' + response.statusText);
+                return;
+            }
+            loadCustomers();
+        })
+        .catch(function(error) {
+            alert(error)
+            createCustomerInProgress = false;
+        })
 };
 
 window.onload = function() {
     loadCustomers();
+    get('new-customer-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        var name = get('new-customer-input').value;
+        reset('new-customer-input');
+        createCustomer(name);
+    });
 };
--- examples/customer-hours-tracker/server.js
+++ examples/customer-hours-tracker/server.js
@@ -11,6 +11,7 @@ const octane = Octane(process.env.OCTANE_API_KEY)
 
 const app = express()
 app.use(express.static('public'))
+app.use(express.json())
 
 app.get('/api/customers', (req, res) => {
     console.log('[octane] Listing customers in account')
@@ -26,10 +27,27 @@ app.get('/api/customers', (req, res) => {
         })
 })
 
+app.post('/api/customers', (req, res) => {
+    const name = req.body.name
+    console.log(`[octane] Attempting to create new customer "${name}"`)
+    octane.customers.create({name})
+        .then(_ => {
+            console.log(`[octane] Customer "${name}" successfully created`)
+            res.status(201)
+            res.send()
+        })
+        .catch(error => {
+            console.error(error)
+            res.status(500)
+            res.send('Internal server error')
+        })
+})
+
 const startServer = () => {
-    const port = process.env.PORT || 3000
-    app.listen(port, () => {
-        console.log(`[server] Listening at http://localhost:${port}/`)
+    const port = process.env.APP_PORT || 3000
+    const bind = process.env.APP_BIND || '127.0.0.1'
+    app.listen(port, bind, () => {
+        console.log(`[server] Listening at http://${bind}:${port}/`)
     })
 }
 
