6bbf57fd6362d5cd62f8b681346d9ad1ba853065
Dima Ivanyuk <dima@getoctane.io>
2021-08-31 17:59:26 -0400

    [oc-node] Remove problematic build lines

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,18 +2,19 @@
 
 set -ex
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $DIR/../
 
 rm -rf build/
 trap "rm -f ${PWD}/tsconfig.tsbuildinfo" EXIT
 tsc -p tsconfig.json
-cp LICENSE build/
-cp README.md build/
+# TODO: Figure out if it's necessary to include these files in the build dir directly.
+# cp LICENSE build/
+# cp README.md build/
 
 # Remove dev-related fields from package.json
-node -e "
-let j=JSON.parse(require('fs').readFileSync('./package.json'))
-delete j['scripts']
-delete j['devDependencies']
-console.log(JSON.stringify(j,null,2))" > build/package.json
+# node -e "
+# let j=JSON.parse(require('fs').readFileSync('./package.json'))
+# delete j['scripts']
+# delete j['devDependencies']
+# console.log(JSON.stringify(j,null,2))" > build/package.json
