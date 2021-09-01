6037d78f6b18c37e972e542337b499b2404f084b
Josh Dolitsky <josh@dolit.ski>
2021-09-01 10:27:48 -0400

    [hotfix] add missing cutsom.d.ts to build
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,19 +2,19 @@
 
 set -ex
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
 
 rm -rf build/
 trap "rm -f ${PWD}/tsconfig.tsbuildinfo" EXIT
 tsc -p tsconfig.json
-# TODO: Figure out if it's necessary to include these files in the build dir directly.
-# cp LICENSE build/
-# cp README.md build/
+cp src/codegen/custom.d.ts build/codegen/ # Fix for bug in v0.5.0
+cp LICENSE build/
+cp README.md build/
 
 # Remove dev-related fields from package.json
-# node -e "
-# let j=JSON.parse(require('fs').readFileSync('./package.json'))
-# delete j['scripts']
-# delete j['devDependencies']
-# console.log(JSON.stringify(j,null,2))" > build/package.json
+node -e "
+let j=JSON.parse(require('fs').readFileSync('./package.json'))
+delete j['scripts']
+delete j['devDependencies']
+console.log(JSON.stringify(j,null,2))" > build/package.json
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../
 
-npm publish --access=public .
+npm publish --access=public build/
