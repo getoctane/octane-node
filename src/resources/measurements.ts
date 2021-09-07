cfbeeba0baca55443916290edf83e2d1c2bcb8cb
Salem Hilal <salemhilal@gmail.com>
2021-09-07 16:36:00 -0400

    Merge src/utils and src/resources/measurements
    All the utils were just used in the measurements resource

--- src/resources/measurements.ts
+++ src/resources/measurements.ts
@@ -1,7 +1,57 @@
 import { Measurement, MeasurementsApi } from '../codegen/api';
 import { Configuration as APIConfiguration } from '../codegen/configuration';
-import { fixMeasurementFields, normalizeMeasurementInput } from '../utils';
-import type { MeasurementInput } from '../utils';
+
+interface MeasurementInput extends Omit<Measurement, 'time'> {
+  time?: Date | string;
+}
+
+/**
+ * This helper augments the body with "meter_name" based on the meterName field.
+ *
+ * TODO: API expects "meter_name", while the generated type uses meterName.
+ * This function should be deleted once the API
+ *
+ * @param param0
+ * @returns
+ */
+function fixMeasurementFields({
+  meterName,
+  ...rest
+}: Measurement): Measurement {
+  return (
+    meterName
+      ? {
+          meter_name: meterName,
+          ...rest,
+        }
+      : {
+          ...rest,
+        }
+  ) as Measurement;
+}
+
+/**
+ * We want to be flexible about the input types we accept, but we also want to
+ * hit our API with a consistent response. This function takes this SDK's
+ * accepted inputs for measurements and normalizes them into the Measurement
+ * type.
+ *
+ * @param measurementInput - A measurementInput object
+ * @returns
+ */
+function normalizeMeasurementInput({
+  time,
+  ...rest
+}: MeasurementInput | Measurement): Measurement {
+  let timestamp: Date | undefined;
+  if (time) {
+    timestamp = time instanceof Date ? time : new Date(time);
+  }
+  return {
+    time: timestamp,
+    ...rest,
+  };
+}
 
 class Measurements {
   private api: MeasurementsApi;
