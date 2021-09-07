719fb0994ce44899c328d0c4e8abea09b99753f2
Salem Hilal <salemhilal@gmail.com>
2021-09-07 16:30:14 -0400

    Allow Measurements to be made with a string Date
    This commit extends the input allowed for measurements.create() to accept an object with a string for the 'time' property. It would be nice to generate something nicer, but that would involve forking OAS-codegen's templates for typescript-fetch, which live here: https://github.com/swagger-api/swagger-codegen/tree/master/modules/swagger-codegen/src/main/resources/typescript-fetch
    
    While we rethink how we generate this SDK, this commit should allow us to at least create some separation between what we accept from users and what we send to the API.

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,43 @@
 import { Measurement } from './codegen/api';
+export interface MeasurementInput extends Omit<Measurement, 'time'> {
+  time?: Date | string;
+}
 
 // TODO: API expects "meter_name", while the type is meterName.
 // This helper augments the body with "meter_name" based
 // on the meterName field.
-const fixMeasurementFields = (m: Measurement) => {
-  if (m.meterName) {
-    // @ts-ignore
-    m.meter_name = m.meterName;
-    // @ts-ignore
-    delete m.meterName;
-  }
-};
+export const fixMeasurementFields = ({
+  meterName,
+  ...rest
+}: Measurement): Measurement =>
+  (meterName
+    ? {
+        meter_name: meterName,
+        ...rest,
+      }
+    : {
+        ...rest,
+      }) as Measurement;
 
-export { fixMeasurementFields };
+/**
+ * We want to be flexible about the input types we accept, but we also want to
+ * hit our API with a consistent response. This function takes this SDK's
+ * accepted inputs for measurements and normalizes them into the Measurement
+ * type.
+ *
+ * @param measurementInput - A measurementInput object
+ * @returns
+ */
+export function normalizeMeasurementInput({
+  time,
+  ...rest
+}: MeasurementInput | Measurement): Measurement {
+  let timestamp: Date | undefined;
+  if (time) {
+    timestamp = time instanceof Date ? time : new Date(time);
+  }
+  return {
+    time: timestamp,
+    ...rest,
+  };
+}
