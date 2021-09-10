2508c8de16615d8b8e9becc2a4ce8bf5bd51f72c
shilal <shilal@etsy.com>
2021-09-10 12:43:09 -0400

    Add tests for measurement's time input
    measurements can have a time property as an ISO string or as a date object, but they should both result in the exact same request.

---
+++ src/resources/__tests__/measurements.test.ts
@@ -0,0 +1,47 @@
+import { Measurements } from 'resources/measurements';
+import fetchMock from 'jest-fetch-mock';
+import type { Measurement } from 'codegen/api';
+
+const API_CONFIG = {
+  apiKey: '867-5309',
+  basePath: 'https://api.made.up.host:1337',
+};
+
+const FAKE_MEASUREMENT: Measurement = {
+  meterName: 'someMeter',
+  value: 9001,
+};
+
+const FAKE_ISO_STRING = '2021-09-10T16:36:59.663Z';
+
+describe('Measurements resource', () => {
+  it('makes create requests with either dates or ISO strings', async () => {
+    const m = new Measurements(API_CONFIG, {});
+
+    // Make one request with time as a string
+    fetchMock.once(JSON.stringify(FAKE_MEASUREMENT));
+    const resp1 = await m.create({
+      ...FAKE_MEASUREMENT,
+      time: FAKE_ISO_STRING,
+    });
+    expect(resp1).toEqual(FAKE_MEASUREMENT);
+    const firstRequest = fetchMock.mock.calls[0];
+
+    // Make one request with time as a date object
+    fetchMock.once(JSON.stringify(FAKE_MEASUREMENT));
+    const resp2 = await m.create({
+      ...FAKE_MEASUREMENT,
+      time: new Date(FAKE_ISO_STRING),
+    });
+    expect(resp2).toEqual(FAKE_MEASUREMENT);
+    const secondRequest = fetchMock.mock.calls[1];
+
+    // Ensure that the outgoing requests are identical, regardless of
+    // whether a date object or an ISO string was passed for `time`
+    expect(firstRequest).toEqual(secondRequest);
+
+    // Snapshot the requests so that we can tell if the generated API
+    // requests ever change.
+    expect(fetchMock.mock.calls).toMatchSnapshot();
+  });
+});
