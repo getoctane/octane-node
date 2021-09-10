f8be15619a3eee0a8507d39b66ccb19a9ab33153
shilal <shilal@etsy.com>
2021-09-10 11:27:52 -0400

    Add Jest tests
    This is a pretty basic set of tests. It makes sure mocked fetch requests get called and resolved as we'd expect, and it snapshots the request that the generated code sends to Fetch.
    This format will let us test both the logic we need in our own wrapper code, *and* it will let us get a better idea about what changes a different generator might introduce.
    
    One or two other fun features:
    1. the tsconfig now includes all TS files, not just the ones in the
       top-level of src (and their imports).
    2. absolute imports work now.

---
+++ src/resources/__tests__/customers.test.ts
@@ -0,0 +1,61 @@
+import { Customers } from 'resources/customers';
+import fetchMock from 'jest-fetch-mock';
+import type { Customer } from 'codegen/api';
+
+const API_CONFIG = {
+  apiKey: '867-5309',
+  basePath: 'https://api.made.up.host:1337',
+};
+
+const FAKE_CUSTOMER: Customer = {
+  name: 'chuck_testa',
+  displayName: 'Chuck Testa',
+  contactInfo: {
+    addressLine1: '1600 Pennsylvania Avenue NW',
+    addressLine2: 'Apt. 4F',
+    city: 'Washington',
+    state: 'DC',
+    zipcode: '20500',
+  },
+};
+
+describe('Customers resource', () => {
+  it('makes create requests', async () => {
+    const c = new Customers(API_CONFIG, {});
+
+    // Call every endpoint
+    fetchMock.once(JSON.stringify(FAKE_CUSTOMER));
+    expect(await c.create(FAKE_CUSTOMER)).toEqual(FAKE_CUSTOMER);
+    expect(fetchMock.mock.calls).toMatchSnapshot();
+  });
+
+  it('makes retrieve requests', async () => {
+    const c = new Customers(API_CONFIG, {});
+    fetchMock.once(JSON.stringify(FAKE_CUSTOMER));
+    expect(await c.retrieve('chuck_testa')).toEqual(FAKE_CUSTOMER);
+    expect(fetchMock.mock.calls).toMatchSnapshot();
+  });
+
+  it('makes update requests', async () => {
+    const c = new Customers(API_CONFIG, {});
+    fetchMock.once(JSON.stringify(FAKE_CUSTOMER));
+    expect(await c.update('chuck_testa', FAKE_CUSTOMER)).toEqual(FAKE_CUSTOMER);
+    expect(fetchMock.mock.calls).toMatchSnapshot();
+  });
+
+  it('makes list requests', async () => {
+    const c = new Customers(API_CONFIG, {});
+    fetchMock.once(JSON.stringify([FAKE_CUSTOMER]));
+    expect(await c.list()).toEqual([FAKE_CUSTOMER]);
+    expect(fetchMock.mock.calls).toMatchSnapshot();
+  });
+
+  it('makes delete requests', async () => {
+    const c = new Customers(API_CONFIG, {});
+    fetchMock.once(JSON.stringify(FAKE_CUSTOMER));
+    expect(await c.delete('chuck_testa').then((res) => res.json())).toEqual(
+      FAKE_CUSTOMER,
+    );
+    expect(fetchMock.mock.calls).toMatchSnapshot();
+  });
+});
