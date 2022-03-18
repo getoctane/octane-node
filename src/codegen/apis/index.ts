793f232f0a9ed7fa7a53fd5e2871e90181c45de6
shilal <salem@getoctane.io>
2022-03-18 17:29:04 -0400

    Run the codegen woooo
    It would be great to not have this committed to our repo. We could commit the openapi.json file to our repo to persist the state of the world, and then add the generator stuff to our build step, but that can come later.

---
+++ src/codegen/apis/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BillingSettingsApi';
+export * from './CouponsApi';
+export * from './CustomerPortalApi';
+export * from './CustomerPortalSettingsApi';
+export * from './CustomersApi';
+export * from './IntegrationsApi';
+export * from './InvoicesApi';
+export * from './MeasurementsApi';
+export * from './MetersApi';
+export * from './PaymentGatewayCredentialApi';
+export * from './PricePlansApi';
+export * from './WebhooksApi';
