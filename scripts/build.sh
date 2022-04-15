fa189faccb2c0f91132bee4bb699b0504e2eef26
shilal <salem@getoctane.io>
2022-04-15 17:24:47 -0400

    Don't ignore /build when publishing
    We used to publish /build directly, but that's not super conventional —  it means having to put package.json, license files, etc. in the build folder.
    It's a lot more common to just publish the whole package and specify the root file in package.json, but to do that we'll need a separate npmignore (.gitignore is used by default) to ensure that out build folder actually gets published.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,16 +8,3 @@ cd $DIR/../
 rm -rf build/
 trap "rm -f ${PWD}/tsconfig.tsbuildinfo" EXIT
 tsc -p tsconfig.json
-
-# Remove test files
-rm -rf build/resources/__tests__/
-
-cp LICENSE build/
-cp README.md build/
-
-# Remove dev-related fields from package.json
-node -e "
-let j=JSON.parse(require('fs').readFileSync('./package.json'))
-delete j['scripts']
-delete j['devDependencies']
-console.log(JSON.stringify(j,null,2))" > build/package.json
