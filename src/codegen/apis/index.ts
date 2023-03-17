48e3d02e86eeecb5541be388b027e15a50ffc238
Vlad Poltorin <vlad@getoctane.io>
2023-03-16 23:03:15 -0400

    chore: update openapi schema

--- src/codegen/apis/index.ts
+++ src/codegen/apis/index.ts
@@ -1,14 +1,18 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './AdminApi';
+export * from './AvalaraApi';
 export * from './BillingSettingsApi';
 export * from './CouponsApi';
+export * from './CreditsApi';
 export * from './CustomerPortalApi';
 export * from './CustomerPortalSettingsApi';
 export * from './CustomersApi';
-export * from './IntegrationsApi';
 export * from './InvoicesApi';
 export * from './MeasurementsApi';
 export * from './MetersApi';
 export * from './PaymentGatewayCredentialApi';
 export * from './PricePlansApi';
+export * from './RefundApi';
+export * from './SelfServeSettingsApi';
 export * from './WebhooksApi';
